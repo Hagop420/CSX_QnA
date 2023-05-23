830f622996742d52f554d7e7698c25620680294e
Hagop420 <jacka161swe@gmail.com>
2023-05-23 16:17:05 -0700

    portfolio

--- csx_challenges.js
+++ csx_challenges.js
@@ -378,3 +378,30 @@ arr.map(function(pl) {
 // Uncomment these to check your work!
 const animals = ["dog", "cat", "tree frog"];
 console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+
+
+
+
+
+
+
+
+
+// callback functions
+
+// ADD CODE HERE
+
+// const map=(num, cb)=>{
+//   let arr=[]
+  
+//    for (let x of num) {
+//     arr.push(cb(x))
+//   }
+//   return arr
+// }
+
+// const subtractTwo=(n)=>n-2
+// // Uncomment these to check your work!
+// console.log(typeof subtractTwo); // should log: 'function'
+// console.log(typeof map); // should log: 'function'
+// console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
