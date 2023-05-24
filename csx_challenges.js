e10a057cee3e32a52bae718b50ea01b6c1b228fe
Hagop420 <jacka161swe@gmail.com>
2023-05-24 12:58:57 -0700

    CSX_challenges_QnA

--- csx_challenges.js
+++ csx_challenges.js
@@ -482,33 +482,33 @@ console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
 
 // does with an if statment
 
-function filterArray(array, callback) {
-  const newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if (callback(array[i])) newArray.push(array[i]);
-  }
-  return newArray;
-}
-const arrOfNums = [1, 2, 3, 4, 5];
-function func1(num) {
-  // return num % 2 === 0;
-  if (num % 2 === 0) {
-   return true;
-  } else {
-    return false;
-  }
+// function filterArray(array, callback) {
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (callback(array[i])) newArray.push(array[i]);
+//   }
+//   return newArray;
+// }
+// const arrOfNums = [1, 2, 3, 4, 5];
+// function func1(num) {
+//   // return num % 2 === 0;
+//   if (num % 2 === 0) {
+//    return true;
+//   } else {
+//     return false;
+//   }
 
-}
-function func2(num) {
-  // return num % 2 !== 0;
-  if (num % 2 !== 0) {
-    return true;
-  } else {
-    return false;
-  }
+// }
+// function func2(num) {
+//   // return num % 2 !== 0;
+//   if (num % 2 !== 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 
-}
+// }
 
-// Uncomment these to check your work!
-console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
-console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
+// // Uncomment these to check your work!
+// console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
