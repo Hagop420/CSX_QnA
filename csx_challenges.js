829d62a943591805b985ad809d01ee64c7ff5926
Hagop420 <jacka161swe@gmail.com>
2023-05-24 12:58:01 -0700

    CSX_challenges_QnA

--- csx_challenges.js
+++ csx_challenges.js
@@ -451,3 +451,30 @@ console.log(map([3, 4, 5], subtractTwo)); // should log: [ 1, 2, 3 ]
 // forEach(['a', 'b', 'c'], i => console.log(i)); // should log: 'a', 'b', 'c'
 // console.log(typeof map); // should log: 'function'
 // console.log(map([3, 4, 5], n => n - 2)); // should log: [1, 2, 3]
+
+
+// dosent return odds and evens in array
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [1, 2, 3, 4, 5];
+function func1(num) {
+  // ADD CODE HERE
+  const evens = numbers.filter((num) => num % 2 === 0);
+  return filterArray(newArray.push(evens))
+}
+
+  
+function func2(num) {
+  // ADD CODE HERE
+  const odds = numbers.filter((num) => num % 2 === 1);
+return filterArray(newArray.push(odds))
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
