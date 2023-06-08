bbcb259120437261dda766d813f07ffff526cfda
Hagop420 <jacka161swe@gmail.com>
2023-06-07 21:03:04 -0700

    Cs_prep_algorithims

--- Cs_prep_algorithims().js
+++ Cs_prep_algorithims().js
@@ -302,3 +302,127 @@ function mode(array) {
  
  // findMissingTests() // Uncomment to test this function
  
+
+
+ ////////////////////////////
+//     Challenge 1
+////////////////////////////
+
+function product(arr, i = 0) {
+   if (i === arr.length - 1) return arr[i];
+   return arr[i] * product(arr, i+1)
+ }
+ 
+ // To check if you've completed the challenge, uncomment these console.logs!
+ // console.log(product([1, 2, 3, 4])); // -> 24
+ // console.log(product([1, 2, 3, 4, 5])); // -> 120
+ // console.log(product([4, 5, 6])); // -> 120
+ 
+ ////////////////////////////
+ //     Challenge 2
+ ////////////////////////////
+ 
+ function containsDuplicates(arr) {
+   const cache = {}
+ }
+ 
+ // To check if you've completed the challenge, uncomment these console.logs!
+ // console.log(containsDuplicates([1, 2, 3, 4, 5, 6])); // -> false
+ // console.log(containsDuplicates([1, 2, 3, 4, 5, 6, 3])); // -> true
+ // console.log(containsDuplicates([])); // -> false
+ 
+ 
+ ////////////////////////////
+ //     Challenge 3
+ ////////////////////////////
+ 
+ function createFib() {
+   const memo = {}
+ 
+   function fib(n) {
+     if (n === 0) return 0;
+     if (n === 1) return 1;
+     // add another base case;
+       if (memo[n] !== undefined) {
+       return memo[n]
+     }
+       
+     let fibn = fib(n-1) + fib(n-2);
+     // what should we do with fibn
+       memo[n] = fibn
+     return fibn
+   }
+ 
+   return fib
+ }
+ 
+ const memoFib = createFib();
+ 
+ // To check if you've completed the challenge, uncomment these console.logs!
+ // console.log(memoFib(0)); // -> 0
+ // console.log(memoFib(5)); // -> 5
+ // console.log(memoFib(37)); // -> 24157817
+ 
+ // console.log(memoFib(45)); // only uncomment if you're sure that memoization is working!
+ 
+ 
+ 
+ ////////////////////////////
+ //     Challenge 4
+ ////////////////////////////
+ 
+ function smallestDiff(arr) {
+   arr.sort((a, b) => {
+     return a - b;
+   });
+ 
+   let smallestDiffValue = Math.max(...arr);
+ 
+   for (let i = 1; i < arr.length; i += 1) {
+     const difference = arr[i] - arr[i - 1];
+     if (difference < smallestDiffValue) {
+       smallestDiffValue = difference;
+     }
+   }
+ 
+   return smallestDiffValue;
+ }
+ 
+ 
+ // To check if you've completed the challenge, uncomment these console.logs!
+ console.log(smallestDiff([4, 1, 3])); // -> 1
+ console.log(smallestDiff([10, 20, 30, 40, 50, 60])); // -> 10
+ console.log(smallestDiff([1, 2, 3, 4, 5, 6, 7])); // -> 1
+ console.log(smallestDiff([0, 2, 5, 10])); // -> 2
+ 
+ 
+ 
+ ////////////////////////////
+ //     Challenge 5
+ ////////////////////////////
+ 
+ function balancedParens(str) {
+   let stack = [];
+   const parensArray = str.split('');
+   for (let i = 0; i < parensArray.length; i++) {
+     const paren = parensArray[i];
+     if (paren === '(') {
+       stack.push(paren);
+     } else if (paren === ')') {
+       if (stack.length === 0) {
+         return false; // Found a closing parenthesis without a matching opening 
+       }
+       stack.pop(); // Matched opening parenthesis found, remove it from stack
+     }
+   }
+ 
+   return stack.length === 0; 
+ }
+ 
+ 
+ // To check if you've completed the challenge, uncomment these console.logs!
+ console.log(balancedParens('()')); // -> true
+ // console.log(balancedParens(')(')); // -> false
+ // console.log(balancedParens('(()(()))')); // -> true
+ // console.log(balancedParens('((()')); // -> false
+ 
