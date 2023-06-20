241160151d2a2d83498867047ca5af6c2a52a208
Hagop420 <jacka161swe@gmail.com>
2023-06-20 13:55:42 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -32,3 +32,18 @@
 // console.log(count);
 // console.log(upper);
 // console.log(lower);
+
+
+
+// 4)const songTitle = "Space Oddity"
+// const lyrics = "This is Ground Control to Major Tom / You’ve really made the grade"
+
+// // ADD CODE BELOW
+// const firstLetter=songTitle[0]
+// const seventhCharacter=songTitle[6]
+// const lastLetter=lyrics.charAt(lyrics.length -1)
+
+// // Uncomment the line below to check your work!
+// console.log(firstLetter);
+// console.log(seventhCharacter);
+// console.log(lastLetter);
