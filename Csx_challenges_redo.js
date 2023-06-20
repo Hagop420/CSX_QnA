ae31f63d9d97919b3cd9c422830f2adb286defa0
Hagop420 <jacka161swe@gmail.com>
2023-06-20 16:53:12 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -126,3 +126,38 @@
 
 // console.log(`I love coding: ${iLoveCoding},
 // Coding is extremely hard: ${codingIsTooHard}`)
+
+
+// 11)// 1.
+// const small = 2;
+// const large = 5342;
+// // ADD CODE BELOW (isSmaller)
+// const isSmaller=small<large
+// console.log(isSmaller)
+
+
+// // 2.
+// const num = 45;
+// const string = "45";
+// // ADD CODE BELOW (isLooselyEqual and isStrictlyEqual)
+
+// const isLooselyEqual=num==string
+// const isStrictlyEqual=num===string
+
+// // 3.
+// const isTrue = true;
+// const isFalse = false;
+// const isTrueNotFalse=isTrue!==isFalse
+// console.log(isTrueNotFalse)
+// // ADD CODE BELOW (isTrueNotFalse)
+
+
+// // Uncomment these to check your work! 
+// console.log('Is 2 < 5342?');
+// console.log(isSmaller);
+// console.log('Is 45 loosely equal to "45"?'); 
+// console.log(isLooselyEqual);
+// console.log('Is 45 strictly equal to "45"?');
+// console.log(isStrictlyEqual);
+// console.log('Is true not equal to false?');
+// console.log(isTrueNotFalse);
