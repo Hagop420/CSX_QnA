96bce5d10b386013c43401a73ef26f1453415b71
Hagop420 <jacka161swe@gmail.com>
2023-06-21 16:07:23 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -256,3 +256,14 @@
 
 
 // console.log(bios)
+
+
+// 19)const increaseByTwo = [1, 2, 3, 4, 5];
+// ADD CODE HERE
+
+
+// Uncomment the line below to check your work!
+// console.log(increaseByTwo); // -> should print [3, 4, 5, 6, 7];
+// for(let i=0; i<increaseByTwo.length; i+=1){
+//    increaseByTwo[i]=increaseByTwo[i]+2
+//    }
