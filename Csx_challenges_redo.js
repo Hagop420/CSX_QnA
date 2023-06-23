1f81ea757def9e974819a6666d272793d79baf0d
Hagop420 <jacka161swe@gmail.com>
2023-06-23 00:58:29 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -361,3 +361,22 @@
 // }
 
 // console.log(checkObj)
+
+// 24)const checkObj = {
+//   oddNum: 1,
+//   evenNum: 2,
+//   foundNum: 5,
+//   randomNum: 18
+// };
+
+// const objToArray = [];
+// // ADD CODE HERE
+
+// for(let jojiComplete in checkObj){
+// 	if(checkObj[jojiComplete]>=2){
+//         // objToArray.push(jojiComplete)
+//     		objToArray.push(checkObj[jojiComplete])
+//   }
+// }
+
+// console.log(objToArray)
