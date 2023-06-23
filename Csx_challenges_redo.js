4b437634361cf0c4a908582746c5ce2e2cbfff2c
Hagop420 <jacka161swe@gmail.com>
2023-06-23 00:39:09 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -330,3 +330,14 @@
 // }
 
 // console.log(fb)
+
+// 22)const timesTenIfOverFive = [23, 9, 11, 2, 10, 6];
+// ADD CODE HERE
+// for(let i=0; i<timesTenIfOverFive.length; i+=1){
+//    if(timesTenIfOverFive[i]>=5){
+//      timesTenIfOverFive[i]*=10
+//    }
+//  }
+ 
+//  // Uncomment the line below to check your work!
+//  console.log(timesTenIfOverFive); // -> should print [230, 90, 110, 2, 100, 60]
