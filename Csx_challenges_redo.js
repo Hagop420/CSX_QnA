2e20f4ea611883c9e56e23bdbf63fd6467c04742
Hagop420 <jacka161swe@gmail.com>
2023-06-24 14:12:12 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -634,3 +634,22 @@ console.log(makePlans("James")) //should return: "Everyone is busy this weekend.
 // console.log(getRemainder(20, 5)); // => 0
 // console.log(getRemainder(8, 22)); // => 6
 // console.log(getRemainder(7, 42)); // => 0
+
+
+// 41)function greetings(hour) {
+  
+//   if(hour<12){
+//    return `Good Morning!`
+//  }else if(hour<15){
+//    return `Good Afternoon!`
+//  }else{
+//    return `Good Night!`
+//  }
+// }
+
+// // Uncomment these to check your work!
+// console.log(greetings(8)); // expected log 'Good Morning!'
+// console.log(greetings(12)); // expected log 'Good Afternoon!'
+// console.log(greetings(14)); // expected log 'Good Afternoon!'
+// console.log(greetings(15)); // expected log 'Good Night!'
+// console.log(greetings(18)); // expected log 'Good Night!'
