0ef704bdefc51dd8d982c43bde25059e80d95a1c
Hagop420 <jacka161swe@gmail.com>
2023-06-24 14:57:14 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -685,3 +685,25 @@
 
 
 // console.log(gradeCalculator(43)); // => "F"
+
+
+
+// 43)function droids(arr) {
+//   let result = '';
+//   // ADD CODE HERE
+//   for(let i=0; i<arr.length; i+=1){
+//    if(arr.includes('Droids')){
+//      result=`Found Droids!`
+//    } else{
+//      result=`These are not the droids you're looking for.`
+//    }
+//   }
+  
+//   return result;
+// }
+
+// // Uncomment these to check your work! 
+// const starWars = ["Luke", "Finn", "Rey", "Kylo", "Droids"] 
+// const thrones = ["Jon", "Danny", "Tyrion", "The Mountain", "Cersei"] 
+// console.log(droids(starWars)) // should log: "Found Droids!"
+// console.log(droids(thrones)) // should log: "These are not the droids you're looking for."
