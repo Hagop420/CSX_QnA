de2e0562d8416c63e2973ab40900cbfa82bb296b
Hagop420 <jacka161swe@gmail.com>
2023-06-24 14:05:20 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -618,3 +618,19 @@ console.log(makePlans("James")) //should return: "Everyone is busy this weekend.
 //    return `I am too normal for odd numbers`
 //  }
 // }
+
+// 40)function getRemainder(num1, num2) {
+  // INSERT CODE HERE
+  
+//   if(num1>num2){
+//    return num1%num2
+//  }else if(num1<num2){
+//    return num2%num1
+//  }
+// }
+
+// // Uncomment the lines below to test your code
+// console.log(getRemainder(17, 5)); // => 2
+// console.log(getRemainder(20, 5)); // => 0
+// console.log(getRemainder(8, 22)); // => 6
+// console.log(getRemainder(7, 42)); // => 0
