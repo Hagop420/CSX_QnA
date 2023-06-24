aa79d98f5f5d67a6bc189f53a3235de77b8f2cb2
Hagop420 <jacka161swe@gmail.com>
2023-06-24 14:00:36 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -603,3 +603,18 @@ console.log(makePlans("James")) //should return: "Everyone is busy this weekend.
 //     return false
 // }
 // }
+
+// 39)// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log(iLoveEvenNumbers(42)); // expected log 'Oh Yeah Evens!'
+// console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers'
+
+
+// function iLoveEvenNumbers(num){
+//  if(num%2===0){
+//    return `Oh Yeah Evens!`
+//  }else{
+//    return `I am too normal for odd numbers`
+//  }
+// }
