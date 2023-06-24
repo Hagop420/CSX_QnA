041695ad22b8c2c68e45b3bdc91cb7b41900fd78
Hagop420 <jacka161swe@gmail.com>
2023-06-23 23:08:08 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -478,3 +478,14 @@
 //  console.log(sayHello('Haley')); // should log: 'Hi, Haley'
  
 
+// 33)function wereAwesome(you, yourBuddy) {
+  // ADD CODE HERE
+//   return `${yourBuddy} and ${you} are awesome!`
+// }
+
+
+// // Uncomment the lines below to test your function:
+// console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+// console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+// console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+// console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
