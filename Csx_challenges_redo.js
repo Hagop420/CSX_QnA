238110b7bdee02778403f36f1bf0a99d7eeff76c
Hagop420 <jacka161swe@gmail.com>
2023-06-24 15:01:16 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -707,3 +707,27 @@
 // const thrones = ["Jon", "Danny", "Tyrion", "The Mountain", "Cersei"] 
 // console.log(droids(starWars)) // should log: "Found Droids!"
 // console.log(droids(thrones)) // should log: "These are not the droids you're looking for."
+
+
+
+// 44)function droids(arr) {
+//   let result = '';
+//   // ADD CODE HERE
+//   arr.forEach(dr=>{
+    
+  
+//    if(dr.includes('Droids')){
+//      result=`Found Droids!`
+//    } else{
+//      result=`These are not the droids you're looking for.`
+//    }
+//   })
+  
+//   return result;
+// }
+
+// // Uncomment these to check your work! 
+// const starWars = ["Luke", "Finn", "Rey", "Kylo", "Droids"] 
+// const thrones = ["Jon", "Danny", "Tyrion", "The Mountain", "Cersei"] 
+// console.log(droids(starWars)) // should log: "Found Droids!"
+// console.log(droids(thrones)) // should log: "These are not the droids you're looking for."
