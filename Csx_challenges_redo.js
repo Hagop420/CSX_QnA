9f5a5b4f143050af4cc4b4d18fda49994240124e
Hagop420 <jacka161swe@gmail.com>
2023-06-25 15:09:17 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -824,3 +824,42 @@
  
 
 
+
+
+// 50)function mergingElements(array1, array2) 
+// {
+//    var result = [];
+//    var ctr = 0;
+//    var x=0;
+ 
+//    if (array1.length === 0) 
+//     return "array1 is empty";
+//    if (array2.length === 0) 
+//     return "array2 is empty";   
+ 
+//   while (ctr < array1.length && ctr < array2.length) 
+//    {
+//      result.push(array1[ctr] + array2[ctr]);
+//      ctr++;
+//    }
+ 
+//   if (ctr === array1.length) 
+//   {
+//      for (x = ctr; x < array2.length; x++)   {
+//        result.push(array2[x]);
+//      }
+//    } 
+//    else
+//    {
+//    for (x = ctr; x < array1.length; x++) 
+//      {
+//        result.push(array1[x]);
+//      }
+//    }
+//    return result;
+//  }
+ 
+//  // Uncomment these to check your work!
+//  console.log(mergingElements([1, 2, 3, 4], [5, 6, 7, 8])); // expected log [6, 8, 10, 12]
+//  console.log(mergingElements([7, 3, 6, 0], [3, 9, 17, 81])); // expected log [10, 12, 23, 81]
+
