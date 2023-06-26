3dc257fdc82fba797ab0e99e05590f31533f9a5a
Hagop420 <jacka161swe@gmail.com>
2023-06-26 13:30:56 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -865,21 +865,20 @@
 
 
 
-// 51)
-  // Adding 2 arrays together
-function mergingTripletsAndQuints(array1, array2) {
-  const c = [];
-  for (let i = 0; Math.max(i < array1.length, i < array2.length); i++) {
-    if (array1[i] % 3 === 0 || array1[i] % 5 === 0) {
-      c.push(array1[i] + array2[i]);
-    } else {
-      c.push(array1[i]);
-    }
-  }
+// 51)// Adding 2 arrays together
+// function mergingTripletsAndQuints(array1, array2) {
+//   const c = [];
+//   for (let i = 0; Math.max(i < array1.length, i < array2.length); i++) {
+//     if (array1[i] % 3 === 0 || array1[i] % 5 === 0) {
+//       c.push(array1[i] + array2[i]);
+//     } else {
+//       c.push(array1[i]);
+//     }
+//   }
 
-        return c;
-}
+//         return c;
+// }
 
-  // Uncomment these to check your work!
-  console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
-  console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
+//   // Uncomment these to check your work!
+//   console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
+//   console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
