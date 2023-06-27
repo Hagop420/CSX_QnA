e53bbbe0fe70030718a310a0bc2854ee2c4b8a8c
Hagop420 <jacka161swe@gmail.com>
2023-06-26 17:28:53 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -882,3 +882,24 @@
 //   // Uncomment these to check your work!
 //   console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
 //   console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
+
+
+function imAboutToExplodeWithExcitement(n) {
+  // ADD CODE HERE
+  
+  while (n >= 0) {
+    console.log(n);
+    if (n === 5) {
+      console.log(`Oh wow, I can't handle the anticipation!`)
+      // continue;
+    } else if (n === 3) {
+      console.log(`I'm about to explode with excitement!`)
+      // continue;
+    }
+    
+    n--
+  }
+}
+
+// Uncomment the line below to check your work!
+imAboutToExplodeWithExcitement(10) // expected log 10, 9, 8, 7, 6, 'Oh wow, I can't handle the anticipation!', 4, I'm about to explode with excitement!', 2, 1, 'That was kind of a let down'
