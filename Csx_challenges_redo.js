467394b71b5572ac7202c0a2d387eea6b8848fd6
Hagop420 <jacka161swe@gmail.com>
2023-07-10 13:03:35 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1001,10 +1001,13 @@
 
 // 57)// ADD CODE HERE 
 
-// const addWaldo=()=>{
-  
-// }
+// ADD CODE HERE 
 
-// // Uncomment these to check your work!
-// const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
-// console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
+// const addWaldo=(n)=>{
+//   n['Waldo']='unknown'
+//     return n
+//   }
+  
+//   // Uncomment these to check your work!
+//   const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+//   console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }Waldo: 'unknown' }
