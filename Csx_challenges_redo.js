1c39da98658cfcb2019beda5928bf7145cbfdd30
Hagop420 <jacka161swe@gmail.com>
2023-07-20 22:15:42 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1055,3 +1055,25 @@
 // // Uncomment these to check your work!
 // const animals = ["dog", "cat", "tree frog"];
 // console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+
+
+// 60)
+
+// ADD CODE HERE
+
+
+
+const map=(cb)=>{
+  return cb.map(min=>{
+    cb[min]-2
+  })
+}
+
+const subtractTwo=()=>{
+  return map
+}
+
+// Uncomment these to check your work!
+console.log(typeof subtractTwo); // should log: 'function'
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
