9f5d0fa19e9b66ff07dc0ee0430f02fa5ec4995d
Hagop420 <jacka161swe@gmail.com>
2023-07-21 17:11:12 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1096,7 +1096,7 @@
 
 // 61)// ADD CODE HERE
 
-
+// loops through current array
 // const forEach=(arr, cb)=>{
 //   let num=[];
 //   for(let i=0; i<arr.length; i+=1){
@@ -1104,6 +1104,8 @@
 //   }
 //   return num
 // }
+
+// end cb looping function
 
 // function map(arr, cb) {
 //   const result = [];
@@ -1118,3 +1120,52 @@
 // forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
 // console.log(typeof map); // should log: 'function'
 // console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+
+
+
+
+
+
+
+// HARD AF
+
+// 61)
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+    // console.log(callback)
+  return newArray;
+}
+const arrOfNums = [1, 2, 3, 4, 5];
+
+
+
+
+function func1(num) {
+  // // ADD CODE HERE
+const nAr=[]
+  if (num % 2 === 0) {
+    num = num;
+  } else {
+    num = null;
+  }
+
+    return num
+}
+function func2(num) {
+  // ADD CODE HERE
+    if (num % 2 !== 0) {
+    num = num;
+  } else {
+    num = null;
+  }
+
+    return num
+
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
