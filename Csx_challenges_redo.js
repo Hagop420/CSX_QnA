98d043d7224f47a25337ab3ad4916faaa301ecfb
Hagop420 <jacka161swe@gmail.com>
2023-07-21 17:11:42 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1129,42 +1129,42 @@
 
 
 // 61)
-function filterArray(array, callback) {
-  const newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if (callback(array[i])) newArray.push(array[i]);
-  }
-    // console.log(callback)
-  return newArray;
-}
-const arrOfNums = [1, 2, 3, 4, 5];
+// function filterArray(array, callback) {
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (callback(array[i])) newArray.push(array[i]);
+//   }
+//     // console.log(callback)
+//   return newArray;
+// }
+// const arrOfNums = [1, 2, 3, 4, 5];
 
 
 
 
-function func1(num) {
-  // // ADD CODE HERE
-const nAr=[]
-  if (num % 2 === 0) {
-    num = num;
-  } else {
-    num = null;
-  }
+// function func1(num) {
+//   // // ADD CODE HERE
+// const nAr=[]
+//   if (num % 2 === 0) {
+//     num = num;
+//   } else {
+//     num = null;
+//   }
 
-    return num
-}
-function func2(num) {
-  // ADD CODE HERE
-    if (num % 2 !== 0) {
-    num = num;
-  } else {
-    num = null;
-  }
+//     return num
+// }
+// function func2(num) {
+//   // ADD CODE HERE
+//     if (num % 2 !== 0) {
+//     num = num;
+//   } else {
+//     num = null;
+//   }
 
-    return num
+//     return num
 
-}
+// }
 
-// Uncomment these to check your work!
-console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
-console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
+// // Uncomment these to check your work!
+// console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
