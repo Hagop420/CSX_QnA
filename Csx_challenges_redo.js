f81fb3f42490fb13bc6c5013f54e17f8d4a3837d
Hagop420 <jacka161swe@gmail.com>
2023-07-24 14:24:38 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1443,3 +1443,21 @@
 // };
 
 // console.log(groupBy(decimals, floored));
+
+
+// 71)// ADD CODE HERE
+// const goodKeys=(obj, callback)=>{
+//   const arr=[]
+//   for(let objectProperties in obj){
+//     const trueFalseVal=callback(obj[objectProperties])
+//     if(trueFalseVal===true){
+//       arr.push(objectProperties)
+//     }
+   
+//   }
+//   return arr
+// }
+// // Uncomment these to check your work!
+// const sunny = { mac: 'priest', dennis: 'calculator', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
+// function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
+// console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
