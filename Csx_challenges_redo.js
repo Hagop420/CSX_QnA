55097b1511672f25771f29aa9b6356d0b2ec8d46
Hagop420 <jacka161swe@gmail.com>
2023-07-25 23:31:08 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1483,3 +1483,27 @@
 // // UNCOMMENT THESE TO TEST YOUR WORK!
 // const sampleFunc = createFunctionWithInput('sample');
 // console.log(sampleFunc()); // should log: 'sample'
+
+
+// 74)function outer() {
+//   let counter = 0; // this variable is outside incrementCounter's scope
+//   function incrementCounter() {
+//     counter++;
+//     console.log('counter', counter);
+//   }
+//   return incrementCounter;
+// }
+
+// const willCounter = outer();
+// const jasCounter = outer();
+
+// // Uncomment each of these lines one by one.
+// // Before your do, guess what will be logged from each function call.
+
+// willCounter();
+// willCounter();
+// willCounter();
+
+// jasCounter();
+// willCounter();
+// jasCounter()
