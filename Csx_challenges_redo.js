7f07aba77554caf2465332f55a30c2820e77fb0e
Hagop420 <jacka161swe@gmail.com>
2023-07-28 00:58:00 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1597,3 +1597,16 @@
 // console.log(getDay()); // should log: 'Sat'
 // console.log(getDay()); // should log: 'Sun'
 // console.log(getDay()); // should log: 'Fri'
+
+
+// 79)// ADD CODE HERE
+// const defineFirstArg=(Func, arg)=>{
+// 	const funcRet=(...addtionalArgs)=>{
+//     return Func(arg, ...addtionalArgs)
+//   }
+//   return funcRet
+// }
+// // Uncomment these to check your work!
+// const subtract = function(big, small) { return big - small; };
+// const subFrom20 = defineFirstArg(subtract, 20);
+// console.log(subFrom20(5)); // should log: 15
