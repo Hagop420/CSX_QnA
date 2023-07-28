18934e5f2f80660cc6b7817d5d59ba97b5eed5f3
Hagop420 <jacka161swe@gmail.com>
2023-07-28 15:04:28 -0700

    CSX_challenges

--- csx_challenges.js
+++ csx_challenges.js
@@ -478,7 +478,7 @@
 // // Uncomment these to check your work!
 // console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
 // console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
-
+// 
 
 // // does with an if statment
 
