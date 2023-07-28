ae7009944e0aaafc7f3d50a3b942f36253afa816
Hagop420 <jacka161swe@gmail.com>
2023-07-28 16:25:16 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1698,3 +1698,21 @@
 // // To check if you've completed the challenge, uncomment these console.logs!
 // console.log(factorial(4)); // -> 24
 // console.log(factorial(6)); // -> 720
+
+
+
+// 83)function getLength(array) {
+ 
+  
+//   if (!("0" in array)) { 
+//     return 0;
+// }
+// return getLength(array.slice(0, -1)) + 1;
+
+// }
+
+// // To check if you've completed the challenge, uncomment these console.logs!
+// console.log(getLength([1])); // -> 1
+// console.log(getLength([1, 2])); // -> 2
+// console.log(getLength([1, 2, 3, 4, 5])); // -> 5
+// console.log(getLength([])); // -> 0
