ae5edba4e317fcee3890e7c9156a7fb0325bf2cc
Hagop420 <jacka161swe@gmail.com>
2023-07-28 22:54:52 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1809,3 +1809,27 @@
 
 // // Uncomment these lines to check your work!
 // console.log(vicky.name); // -> Logs 'Vi
+
+
+// 89)const personStore = {
+//   greet: function() {
+//     console.log('hello');
+//   }
+// }
+
+// function personFromPersonStore(name, age) {
+// 	// add code here
+//   const person = Object.create(personStore);
+//   person.name = name;
+//   person.age = age;
+
+//   return person;
+
+// }
+
+// const sandra = personFromPersonStore('Sandra', 26);
+
+// // Uncomment these lines to check your work!
+// console.log(sandra.name); // -> Logs 'Sandra'
+// console.log(sandra.age); // -> Logs 26
+// sandra.greet(); // -> Logs 'hello'
