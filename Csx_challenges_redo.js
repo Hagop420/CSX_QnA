30327075ab7273ec0c4f1f42cbe633df58d4a6a7
Hagop420 <jacka161swe@gmail.com>
2023-07-28 23:07:54 -0700

    CSX_challenges_redo

--- Csx_challenges_redo.js
+++ Csx_challenges_redo.js
@@ -1833,3 +1833,37 @@
 // console.log(sandra.name); // -> Logs 'Sandra'
 // console.log(sandra.age); // -> Logs 26
 // sandra.greet(); // -> Logs 'hello'
+
+
+
+
+
+// 90)const personStore = {
+//   greet: function() {
+//     console.log('hello');
+//   },
+//     introduce: function() {
+//     console.log(`Hi, my name is ${sandra.name}`)
+
+//   }
+
+// }
+
+
+
+// function personFromPersonStore(name, age) {
+// 	const person = Object.create(personStore);
+ 
+//   person.name = name;
+//   person.age = age;
+ 
+//   return person;
+// }
+
+// const sandra = personFromPersonStore('Sandra', 26);
+
+// // add code here
+
+
+// // Uncomment this line to check your work!
+// sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
